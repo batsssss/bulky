a8d569fe8a793f3aff5d9c598d0357a829af6d25
Barbara Goss <barbara.goss@synthose.com>
2019-12-04 19:16:58 -0500

    output weighing form details

--- resources/js/pages/weighing-form/order-item.js
+++ resources/js/pages/weighing-form/order-item.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import ProductPack from './product-pack';
-import {Typography} from "@material-ui/core";
+import {List, ListSubheader, Typography} from "@material-ui/core";
 
 function OrderItem(props) {
-    const {orderItem} = props;
+    const {orderItem, packagings} = props;
     return (
-        <div>
-          <Typography align="left">
-            Order {orderItem.order.order_num}
-            - {orderItem.size}mg
-            x {orderItem.quantity}
-            = {orderItem.size * orderItem.quantity}mg
-          </Typography>
+        <React.Fragment>
+          <List subheader={
+            <ListSubheader disableSticky={true}>
+              <Typography align="left">
+                Order {orderItem.order.order_num}
+                - {orderItem.size}mg
+                x {orderItem.quantity}
+                = {orderItem.size * orderItem.quantity}mg
+              </Typography>
+            </ListSubheader>
+          }>
             {orderItem.product_packs.map((productPack, index) =>
-                <div key={"orderItem" + orderItem.id + "productPack" + index}>
-                  <ProductPack
-                      productPack={productPack}
-                      index={index} />
-                </div>
+                <ProductPack
+                    key={"orderItem" + orderItem.id + "productPack" + index}
+                    productPack={productPack}
+                    index={index}
+                    packagings={packagings}
+                />
             )}
-        </div>
+          </List>
+        </React.Fragment>
     )
 }
 
